aacdf1e6933f027e7813a241a6d8c3a6bc659b37
kenfzhang <kzhang395@gmail.com>
2017-04-21 00:25:21 -0500

    wat

--- Projects/STM32F411E-Discovery/Examples/I2C/ECE445_MasterCode/Src/main.c
+++ Projects/STM32F411E-Discovery/Examples/I2C/ECE445_MasterCode/Src/main.c
@@ -35,7 +35,7 @@
   * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
   * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   *
-  * But really though, who would want to steal this?
+  * 
   ******************************************************************************
   */
 
